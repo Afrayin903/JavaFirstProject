870bcf3377f49080436a4b40318cfa9afa8c136c
Kahar <afrayin0222@gmail.com>
2025-11-06 14:06:40 -0500

    Added new codes

--- src/practice2/Employee.java
+++ src/practice2/Employee.java
@@ -1,10 +1,10 @@
 package practice2;
 
 public class Employee {
-    public String name;
-    public char gender;
-    public String jobTitle;
-    public double Salary;
+    private String name;
+    private char gender;
+    private String jobTitle;
+    private double Salary;
 
     Employee(String name, char gender, String jobTitle, double salary){
         this.name = name;
@@ -12,6 +12,44 @@ public class Employee {
         this.jobTitle = jobTitle;
         this.Salary = salary;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public double getSalary() {
+        return Salary;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public void setSalary(double salary) {
+         if(salary<=0) {
+             System.out.println("Invalid salary " + salary);
+             System.exit(0);
+         }
+        this.Salary = salary;
+
+    }
+
     @Override
     public String toString() {
         return "Employee{" +
--- src/practice2/EmployeeObjects.java
+++ src/practice2/EmployeeObjects.java
@@ -4,5 +4,10 @@ public class EmployeeObjects {
     public static void main(String[] args) {
         Employee employee = new Employee("Kahar", 'M', "SDET", 100000);
         System.out.println(employee);
+
+        System.out.println("==============================");
+        employee.setName("Nihat");
+        employee.setSalary(-120000);
+        System.out.println("He is name " + employee.getName() + " and salary is more than " +employee.getSalary());
     }
 }
