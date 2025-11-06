bee8be7d7b5b63dc5ff3a7bbeb5e6f60d2842e62
Kahar <afrayin0222@gmail.com>
2025-11-06 10:47:18 -0500

    Added updated classes

--- src/practice/IteratorArrayList.java
+++ src/practice/IteratorArrayList.java
@@ -1,10 +1,14 @@
 package practice;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class IteratorArrayList {
     public static void main(String[] args) {
         ArrayList <Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(3);
+        list.add(4);
         list.add(10);
         list.add(30);
         list.add(40);
@@ -20,5 +24,23 @@ public class IteratorArrayList {
             System.out.print(" " + list.get(i));
         }
         //list.fori keyword to populate for loop
+        //for each loop
+        System.out.println("\n==========================");
+        //Type list.for automatically get foreach loop
+        for (Integer each : list) {
+            System.out.print(" "+each);
+        }
+        System.out.println("\n=============================");
+        //RemoveIf(Predicate): Remove elements from the list if they are not matching with condition.
+        list.removeIf(each -> each<10);
+        System.out.println(list);
+
+        System.out.println("\n================================");
+        //removeIf method tests on string
+        ArrayList <String> names = new ArrayList<>();
+        names.addAll(Arrays.asList("Kahar", "Kasim", "Kamar", "Anker", "Afrayin", "Gulnur"));
+        names.removeIf(p->p.startsWith("K"));
+        System.out.println(names);
+
     }
 }
